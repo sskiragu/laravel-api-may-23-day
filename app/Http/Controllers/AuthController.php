9c2d4553412497f0038b944fa5e38ed65b1ec04a
sskiragu <skiragu@gmail.com>
2024-03-12 16:22:09 +0300

    added login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -14,5 +15,18 @@ class AuthController extends Controller
         User::create($user_input);
 
         return "User created";
+    }
+
+    // login
+    function login(Request $request){
+    
+        $user_input = $request->only(['email','password']);
+
+        if (Auth::attempt($user_input)) {
+            return "Succesful login";
+        }else{
+            return "Invalid credentials";
+        }
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/signup',[AuthController::class, 'signup']);
+Route::post('/login',[AuthController::class, 'login']);
